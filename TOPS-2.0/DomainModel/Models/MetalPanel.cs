996d84b49d25b582a8d7559e214514a85ab37274
chris <cjaler@gmail.com>
2024-11-11 12:21:51 -0600

    created MetalPanel class

--- TOPS-2.0/DomainModel/Models/MetalPanel.cs
+++ TOPS-2.0/DomainModel/Models/MetalPanel.cs
@@ -8,5 +8,57 @@ namespace DomainModel.Models
 {
 	public class MetalPanel
 	{
+		public int Id
+		{
+			get; set;
+		}
+		public string Substrate
+		{
+			get; set;
+		}
+		public float Thickness
+		{
+			get; set;
+		}
+		public string Finish
+		{
+			get; set;
+		}
+		public float Width
+		{
+			get; set;
+		}
+		public float Height
+		{
+			get; set;
+		}
+		public string Framing
+		{
+			get; set;
+		}
+		public string InstallHardware
+		{
+			get; set;
+		}
+		public bool IsSplit
+		{
+			get; set;
+		}
+		public string AdditionalHardware
+		{
+			get; set;
+		}
+
+		public MetalPanel(){}
+		public MetalPanel(string sub,float thick, string fin,float w,float h,int? id = -1)
+		{
+			if (id != -1)
+				Id = (int) id;
+			Substrate = sub;
+			Thickness = thick;
+			Finish = fin;
+			Width = w;
+			Height = h;
+		}
 	}
 }
