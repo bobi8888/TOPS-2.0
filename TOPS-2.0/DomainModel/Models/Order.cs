ed979950d79b74643394abe0972665589327701a
chris <cjaler@gmail.com>
2024-11-11 12:09:41 -0600

    Created Order class

--- TOPS-2.0/DomainModel/Models/Order.cs
+++ TOPS-2.0/DomainModel/Models/Order.cs
@@ -8,9 +8,42 @@ namespace DomainModel.Models
 {
 	internal class Order
 	{
+		int Id
+		{
+			get; set;
+		}
+		string Sidemark
+		{
+			get; set;
+		}
+		string OrderId
+		{
+			get; set;
+		}
+		bool InProduction
+		{
+			get; set;
+		}
+		bool IsShipped
+		{
+			get; set;
+		}
+		string CreationDate
+		{
+			get; set;
+		}
 		string DeliveryAddress
 		{
 			get; set;
+		}
+
+		public Order(){}
+		public Order(string sm, string orderId, int? id = -1){
+			if (id != -1)
+				Id = (int) id;
+			Sidemark = sm;
+			OrderId = orderId;
+			//create the logic to assign a creation date
 		}
 	}
 }
