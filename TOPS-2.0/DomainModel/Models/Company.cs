a527b8991d436f1464b5234d3fbefd7c8b40df2a
chris <cjaler@gmail.com>
2024-11-11 12:29:57 -0600

    created Company class

--- TOPS-2.0/DomainModel/Models/Company.cs
+++ TOPS-2.0/DomainModel/Models/Company.cs
@@ -8,5 +8,40 @@ namespace DomainModel.Models
 {
 	public class Company
 	{
+		public int Id { get; set; }
+		public string Name
+		{
+			get; set;
+		}
+		public string Type
+		{
+			get; set;
+		}
+		public string State
+		{
+			get; set;
+		}
+		public string Address
+		{
+			get; set;
+		}
+		public string Zip
+		{
+			get; set;
+		}
+		public float Discount
+		{
+			get; set;
+		}
+
+		public Company(){}
+		public Company(string name, string type, int? id = -1)
+		{
+			if (id != -1)
+				Id = (int) id; 
+			Name = name; 
+			Type = type;
+		}
+
 	}
 }
--- TOPS-2.0/DomainModel/obj/Debug/net8.0/DomainModel.AssemblyInfo.cs
+++ TOPS-2.0/DomainModel/obj/Debug/net8.0/DomainModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+594aa2a95afbe84d70137e95fdf141d7a2561d2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996d84b49d25b582a8d7559e214514a85ab37274")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
